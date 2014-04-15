3de69386635fb5c7b24625db8a4a4cda9fdbdf2d
Paul Wenig <pwenig@gmail.com>
2014-04-15 15:10:06 -0600

    [#69485904] Styled register page.
    Fixed style on 'not logged in' index page. Updated spec file to reflect style changes
    
    Signed-off-by: Emily Platzer <emilyplatzer@gmail.com>

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -7,8 +7,8 @@ feature 'Homepage' do
   scenario 'User can register' do
     visit '/'
     click_on 'Register'
-    fill_in 'Email', with: 'bob@example.com'
-    fill_in 'Password', with: 'password'
+    fill_in 'email', with: 'bob@example.com'
+    fill_in 'password', with: 'password'
     click_on 'Register'
     expect(page).to have_content 'Welcome, bob@example.com'
   end
